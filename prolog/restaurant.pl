3d651a517cd57e034cce57015591bd7b899fc338
Onk Ar <kocarond@gmail.com>
2026-02-26 16:46:48 +0100

    Added task text to the restaurant riddle

--- prolog/restaurant.pl
+++ prolog/restaurant.pl
@@ -1,3 +1,37 @@
+/* Task: 
+Write a Prolog program that can solve the following logic puzzle.
+
+David, Emma, Stella, and Thomas were at a restaurant in Prague. 
+They sat at a square table, with one person on each side, and the 
+men sat across the table from each other, as did the women. 
+Each of them ordered a different food along with a different beverage. 
+Also:
+
+    The person with cider sat across from the person with trout.
+    The dumplings came with beer.
+    The mushroom soup came with cider.
+    The person with pasta sat across from the person with beer.
+    David never drinks iced tea.
+    Emma only drinks wine.
+    Stella does not like dumplings.
+
+Who ordered which food?
+
+Your program should include a predicate 'solve' that takes four arguments. 
+solve(Dumplings, Pasta, Soup, Trout) should return the names of the people 
+who ordered each dish. Names are lowercase Prolog atoms. For example, 
+if rule 7 above were absent, the output might begin like this:
+
+?- solve(Dumplings, Pasta, Soup, Trout).
+Dumplings = stella,
+Pasta = emma,
+Soup = david,
+Trout = thomas ;
+...
+
+Of course, your output will be different, since with rule 7 the above output is not a valid solution.
+ */
+
 man(david).
 man(thomas).
 
