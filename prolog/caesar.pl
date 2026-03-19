11289833164e65e746dec4f156578e86a0dd0e93
Onk Ar <kocarond@gmail.com>
2026-03-19 20:39:06 +0100

    Added task description

--- prolog/caesar.pl
+++ prolog/caesar.pl
@@ -1,3 +1,26 @@
+/*Task:In a simple Caesar cipher, each letter is rotated forward in the alphabet by some number of 
+places K. For example, if we encrypt the word 'YES' with K = 3 then we obtain 'BHV'. If the key K 
+is known, the cipher can be decrypted by rotating each letter by the inverse value -K.
+
+In this assignment, we will consider a slightly more complicated cipher in which there are two keys 
+K1 and K2. To encrypt each word, all letters at odd positions in the word are rotated by K1, and 
+letters at even positions are rotated by K2. For example, if K1 = 1 and K2 = 6 and we encrypt the 
+word "DONUT", we obtain "EUOAU", because
+
+   D + 1 = E
+   O + 6 = U
+   N + 1 = O
+   U + 6 = A
+   T + 1 = U
+
+Write a predicate decrypt(+C, +D, -M) that takes an encrypted text C and a list of words D, and 
+produces the original text M. All words in the text have been encrypted with the same keys K1 and K2. 
+Their values are not known, however it is guaranteed that every word in the decrypted text will be 
+in the given list D. All words will contain only lowercase letters in the range 'a' .. 'z'.
+
+If there is more than one possible answer, your predicate should succeed once for each possibility.
+*/
+
 decrypt(C, D, M) :- 
     split_string(C, " ", " ", EncryptedWords),
     decrypt_all(EncryptedWords, D, DecryptedWords, _, _),
