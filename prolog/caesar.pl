82fb2c224b3cedb8aabe847b918fe5f853e692f3
Onk Ar <kocarond@gmail.com>
2026-03-19 20:22:47 +0100

    Added a predicate to decrypt a single word

---
+++ prolog/caesar.pl
@@ -0,0 +1,15 @@
+decrypt(C, D, M) :- 
+    split_string(C, " ", " ", EncryptedWords).
+
+decrypt_word(E, D, K1, K2) :-
+    string_codes(E, ECodes),
+    string_codes(D, DCodes),
+    decrypt_codes(ECodes, DCodes, K1, K2).
+
+decrypt_codes([], [], _, _). 
+decrypt_codes([E], [D], K, _) :-
+    K is (E - D) mod 26.
+decrypt_codes([E1, E2 | ETail], [D1, D2 | DTail], K1, K2) :-
+    K1 is (E1 - D1) mod 26,
+    K2 is (E2 - D2) mod 26,
+    decrypt_codes(ETail, DTail, K1, K2).
