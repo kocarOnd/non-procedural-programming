c3790cfb21d8798175a40163565a30de092c3bfb
Onk Ar <kocarond@gmail.com>
2026-05-15 11:32:00 +0200

    Implemented the tautology assignment

---
+++ haskell/tautology.hs
@@ -0,0 +1,34 @@
+import Data.Maybe (fromJust)
+import Data.List (nub)
+
+data Prop =
+    Const Bool
+  | Var Char
+  | Not Prop
+  | And Prop Prop
+  | Or Prop Prop
+  deriving Show
+
+type Subst = [(Char, Bool)]
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b) = b
+eval subs (Var a) = fromJust (lookup a subs) {-Assumes a always in subs-}
+eval subs (Not p) = not (eval subs p)
+eval subs (And p1 p2) = eval subs p1 && eval subs p2
+eval subs (Or p1 p2) = eval subs p1 || eval subs p2
+
+vars :: Prop -> [Char]
+vars (Const a) = []
+vars (Var a) = [a]
+vars (Not a) = vars a
+vars (Or a b) = nub (vars a ++ vars b)
+vars (And a b) = nub (vars a ++ vars b)
+
+substs :: [Char] -> [Subst]
+substs [] = [[]]
+substs (x:xs) = [ (x, b) : s | s <- substs xs, b <- [True, False]]
+
+isTaut :: Prop -> Bool
+isTaut p = all (`eval` p) s
+    where s = substs (vars p)
