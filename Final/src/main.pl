acb03c2b85d60447cf08d7e417ee97e7f6be4460
Onk Ar <kocarond@gmail.com>
2026-06-29 20:25:51 +0200

    Created functionality for morning and evening constraints, created lunch checking

--- Final/src/main.pl
+++ Final/src/main.pl
@@ -1,6 +1,7 @@
 :- use_module(data).
 :- use_module(library(clpfd)).
 :- use_module(view).
+:- use_module(settings).
 
 /*TURNING ATOMS TO INTEGERS*/
 
@@ -23,6 +24,91 @@ travel_time(Building, Building, 0).
 travel_time(A, B, Time) :- distance(A, B, Time). 
 travel_time(A, B, Time) :- distance(B, A, Time). 
 
+morning_condition(SHour) :-
+    is_morning_avoided(Limit), !,
+    SHour >= Limit.
+morning_condition(_) :- 
+    true.
+
+evening_condition(EHour, Eminute) :-
+    is_evening_avoided(Limit), !,
+    (
+        EHour < Limit;
+        (EHour =:= Limit, Eminute =:= 0)
+    ).
+evening_condition(_, _) :-
+    true.
+
+check_lunch_breaks(Schedule) :-
+    is_lunch_necessary, !,
+    maplist(find_info, Schedule, ExpandedSchedule),
+
+    maplist(extract_day, ExpandedSchedule, AllDays),
+    sort(AllDays, UniqueDays),
+
+    check_valid_days(UniqueDays, ExpandedSchedule).
+check_lunch_breaks(_) :-
+    true.
+
+
+find_info(Name-GroupID, [Name, GroupID, Day, Stime, Etime, Building]) :-
+    course(Name, GroupID, Day, Shour:Smin, Ehour:Emin, Room),
+    Stime is Shour * 60 + Smin,
+    Etime is Ehour * 60 + Emin,
+    room(Building, Room).
+
+extract_day([_, _, Day, _, _, _], Day).
+
+check_valid_days([], _).
+check_valid_days([Day | Days], ExpandedSchedule) :-
+    findall(Class, 
+        (
+            member(Class, ExpandedSchedule), 
+            Class = [_, _, Day, _, _, _]
+        ), 
+        DailyClasses),
+    sort(4, @=<, DailyClasses, SortedClasses),
+
+    (
+        (
+            SortedClasses = [[_, _, _, Stime, Etime, _]],
+            (Stime >= 630; Etime =< 810)
+        );
+        (
+            SortedClasses = [_ | Rest],
+            any(check_pair_gap, SortedClasses, Rest)
+        ); 
+        (
+            SortedClasses = [FirstClass | _],
+            FirstClass = [_, _, _, Stime, _, _],
+            Stime >= 630
+        );
+        (
+            last(SortedClasses, LastClass),
+            LastClass = [_, _, _, _, Etime, _],
+            Etime =< 810
+        )
+    ),
+    check_valid_days(Days, ExpandedSchedule).
+
+any(Pred, [X|_], [Y|_]) :-
+    call(Pred, X, Y),
+    !. 
+any(Pred, [_|Xs], [_|Ys]) :-
+    any(Pred, Xs, Ys).
+
+check_pair_gap(Class1, Class2) :- 
+    Class1 = [_, _, _, _, Etime, Bldg1],
+    Class2 = [_, _, _, Stime, _, Bldg2],
+
+    travel_time(Bldg1, Bldg2, TravelTime),
+
+    Etime < 810, % First class must end before 13:30 
+    Stime >= 630, % Second class must start after 10:30
+
+    RestTime is Stime - Etime - TravelTime,
+    RestTime >= 30.
+
 /*TURNING DATA TO CSP*/
 
 /*Build table of travel distances*/
@@ -44,6 +130,9 @@ course_tuples(Name, Tuples) :-
         [ID, Start, End, BldgInt], 
         (
             course(Name, ID, Day, StartH:StartM, EndH:EndM, Room),
+
+            morning_condition(StartH),
+            evening_condition(EndH, EndM),
             
             room(BldgName, Room),
             building_id(BldgName, BldgInt),
@@ -61,7 +150,7 @@ all_courses(Courses) :-
 
 /*Give course its domain*/
 course_domain(Name, Name-Var) :-
-    findall(GroupId, course(Name, GroupId, _, _, _, _), Groups),
+    findall(GroupID, course(Name, GroupID, _, _, _, _), Groups),
     max_list(Groups, MaxGroup),
     Var in 1..MaxGroup.
 
@@ -102,6 +191,8 @@ solve(Schedule) :-
     valid_schedule(Schedule),
     
     labeling([], VarsOnly),
+
+    check_lunch_breaks(Schedule),
 
     sort_schedule(Schedule, SortedSchedule),
 
